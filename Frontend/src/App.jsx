2a76957785f61d97008266942d0b7670bdd84771
sayanophile <chakrabortysayan922@gmail.com>
2025-09-27 11:05:37 +0530

    Added Profile.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-
+import Profile from "./components/Profile.jsx"
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     This is an Influencer Profile Page
+     <Profile/>
     </>
   )
 }
